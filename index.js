363c9f862494aca0d6a716337e10081215020615
zemmelmootez <zemmel.mootez@gmail.com>
2022-04-10 17:32:30 +0100

    hello

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function test(){
    
     if(document.body.style.backgroundColor=="white")
@@ -5,13 +6,33 @@ function test(){
     var all = document.getElementsByTagName("*");
 
     for (var i=0, max=all.length; i < max; i++) {
+      
      all[i].style.color = "white";
+     
     }
 }
     else { document.body.style.backgroundColor="white";
     var all = document.getElementsByTagName("*");
 
     for (var i=0, max=all.length; i < max; i++) {
+    
+      
      all[i].style.color = "black";
     }}
+}
+*/
+function test()
+{
+  
+ if( document.getElementById("switch").attributes[3].value=="dark")
+  {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    document.getElementById("switch").attributes[3].value="light";
+    document.getElementById("logo").src="logodark.png";
+}
+else
+{ document.getElementById("switch").attributes[3].value="dark";
+    document.documentElement.setAttribute('data-theme', 'light');
+    document.getElementById("logo").src="Groupe 17.png";
+}
 }
