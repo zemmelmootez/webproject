d91be8eb6faeddb454639bd22126c24d422177eb
zemmelmootez <zemmel.mootez@gmail.com>
2022-04-17 18:38:51 +0100

    o

--- index.js
+++ index.js
@@ -65,3 +65,11 @@ $(document).ready(function(){
       } // End if
     });
   });
+  function myFunction() {
+    var x = document.getElementById("top");
+    if (x.className === "header") {
+      x.className += " responsive";
+    } else {
+      x.className = "header";
+    }
+  }
